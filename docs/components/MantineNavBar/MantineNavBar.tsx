0089810d9829526bbc66d55b3e558795eb61649f
nullmatrix0 <nullmatrixz@proton.me>
2025-12-05 05:53:09 +0700

    chore: update README and docs link

--- docs/components/MantineNavBar/MantineNavBar.tsx
+++ docs/components/MantineNavBar/MantineNavBar.tsx
@@ -1,7 +1,8 @@
 'use client';
 
+import Image from 'next/image';
 import { Navbar } from 'nextra-theme-docs';
-import { Group, Text, useMantineColorScheme } from '@mantine/core';
+import { Group, Text, useMantineColorScheme, Anchor } from '@mantine/core';
 import { ColorSchemeControl } from '../ColorSchemeControl/ColorSchemeControl';
 import { Logo } from '../Logo/Logo';
 import { MantineNextraThemeObserver } from '../MantineNextraThemeObserver/MantineNextraThemeObserver';
@@ -28,7 +29,23 @@ export const MantineNavBar = () => {
         }
         projectLink="https://github.com/zaunchpad/zaunchpad"
       >
-        <ColorSchemeControl />
+        <Group gap="md">
+          <Anchor
+            href="https://x.com/zaunchpad"
+            target="_blank"
+            rel="noopener noreferrer"
+            style={{ textDecoration: 'none', display: 'flex', alignItems: 'center' }}
+          >
+            <Image
+              src={colorScheme === 'light' ? '/assets/x-light.png' : '/assets/x-dark.png'}
+              alt="X (Twitter)"
+              width={34}
+              height={34}
+              style={{ objectFit: 'contain' }}
+            />
+          </Anchor>
+          <ColorSchemeControl />
+        </Group>
       </Navbar>
     </>
   );
